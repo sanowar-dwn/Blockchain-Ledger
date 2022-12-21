015975a408d9a0a6cf9b95942c0ec7ba8ac60bca
Sanowar Dewan <sanowardwnofficial@gmail.com>
2022-12-21 21:21:42 +0000

    new project

--- travelloapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Destination(models.Model):
-    name = models.CharField(max_length=20)
-    desc = models.TextField()
-    price = models.IntegerField()
-    img = models.ImageField(upload_to='pics')
-    offer = models.BooleanField(default=False)
--- travelloapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Destination
-# Create your views here.
-
-
-def index(request):
-    dest = Destination.objects.all()
-    return render(request, 'index.html', {'dest': dest})
